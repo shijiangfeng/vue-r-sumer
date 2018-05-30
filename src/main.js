90edb501d31c3a48c66905cb8e780f815c457eee
shijiangfeng <512160600@qq.com>
2018-05-30 19:32:35 +0800

    add

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import 'normalize.css' //让每个浏览器css都初始化 保持一样
 import ElementUI from 'element-ui'; //饿了么
-import 'element-ui/lib/theme-chalk/index.css'; //饿了么css样式
+import 'normalize.css' //让每个浏览器css都初始化 保持一样
+import 'element-ui/lib/theme-chalk/index.css'; //饿了么css样式\
+import App from './App'
 import './assets/reset.scss' //
 
 Vue.use(ElementUI); //vue使用element ui
